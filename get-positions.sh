86778c706373029b017389a21910c777c8f91712
Patrick Violette <violette.patrick718@gmail.com>
2014-02-26 15:01:46 -0500

    tsv instead of \tsv and run prediction requests as subprocesses

--- get-positions.sh
+++ get-positions.sh
@@ -7,7 +7,7 @@ do
     buses=`curl -s http://m.cip.gatech.edu/api/buses/position | jq --raw-output '.[] | "\(.jobID)\t\(.speed)\t\(.plng)\t\(.plat)\t\(.lng)\t\(.lat)\t\(.color)\t\(.id)"'`
     for bus in $buses
     do
-        echo "$d\t$bus"
+        echo -e "$d\t$bus"
     done
     sleep 50s
 done
--- get-predictions.sh
+++ get-predictions.sh
@@ -8,7 +8,7 @@ while true; do
         stop=`echo "$line" | cut -d "-" -f 2`
         echo "finding $route/$stop"
         [ -d "$root/$route" ] || mkdir -p "$root/$route"
-        curl -s "http://m.cip.gatech.edu/api/buses/predict/$route?stop=$stop" | jq --raw-output 'select(.query.results) | "\(.query.created)\t\(.query.results.p)\t\(.query.results.buses)"' >> "$root/$route/$stop.tsv"
+        curl -s "http://m.cip.gatech.edu/api/buses/predict/$route?stop=$stop" | jq --raw-output 'select(.query.results) | "\(.query.created)\t\(.query.results.p)\t\(.query.results.buses)"' >> "$root/$route/$stop.tsv" &
     done
     sleep 3m
 done
